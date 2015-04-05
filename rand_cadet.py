a4a941a88f43fb607f5768f287230fb216b0f23e
oronnya <oronnya@yahoo.com>
2015-04-04 18:45:03 -0700

    code cleaned. data structure changed to dict

--- rand_cadet.py
+++ rand_cadet.py
@@ -2,9 +2,30 @@
 import random
 
 all_cadets = range(848, 900)
-
-excluded = [849, 850, 851, 857, 899]
+print(len(all_cadets))
+excluded = [849, 850, 851, 857, 867, 899]
 
 current = [x for x in all_cadets if x not in excluded]
+# use_dictionary?????
+nexts = []
+temps = []
 
-print(random.choice(current))
+#http://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+import operator
+
+# dict by cadet number
+d = {}
+
+for i in range(300):
+    r = random.choice(current)
+    
+    if r in d.keys():
+        d[r] = d[r] + 1
+        
+    else:
+        d[r] = 1
+
+#dict.iteritems() is removed in Py 3, use .items instead
+#http://stackoverflow.com/questions/13998492/iteritems-in-python
+chosen = max(d.items(), key=operator.itemgetter(1))[0]   
+print(chosen)
